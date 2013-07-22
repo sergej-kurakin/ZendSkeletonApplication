b07da9cd4f4754fc4d27ef9e69778cdb04e301ff
Sergej Kurakin <sergej@kurakin.info>
2013-07-22 22:33:08 +0300

    Add Zend Framework 2 Developer Tool

--- config/application.config.php
+++ config/application.config.php
@@ -5,6 +5,10 @@ $modules = array();
 
 $modules[] = 'Application';
 
+if ($env == 'development') {
+    $modules[] = 'ZendDeveloperTools';
+}
+
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => $modules,
