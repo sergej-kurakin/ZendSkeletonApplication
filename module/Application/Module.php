888dd38829da0b3fccb87d1e3086049931dc7308
Sergej Kurakin <sergej@kurakin.info>
2013-07-22 23:55:27 +0300

    Add simple console banner and console help

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -14,14 +14,19 @@ use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\BootstrapListenerInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\ModuleManager\Feature\ConsoleBannerProviderInterface;
+use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
+use Zend\Console\Adapter\AdapterInterface;
 
 class Module implements
     BootstrapListenerInterface,
     ConfigProviderInterface,
     AutoloaderProviderInterface,
-    ServiceProviderInterface
+    ServiceProviderInterface,
+    ConsoleBannerProviderInterface,
+    ConsoleUsageProviderInterface
 {
 
     /**
@@ -119,6 +124,50 @@ class Module implements
             $level++;
         }
         while($exception = $exception->getPrevious());
+    }
+
+    /**
+     * Returns a string containing a banner text, that describes the module and/or the application.
+     * The banner is shown in the console window, when the user supplies invalid command-line parameters or invokes
+     * the application with no parameters.
+     *
+     * The method is called with active Zend\Console\Adapter\AdapterInterface that can be used to directly access Console and send
+     * output.
+     *
+     * @param AdapterInterface $console
+     * @return string|null
+     */
+    public function getConsoleBanner(AdapterInterface $console)
+    {
+        return 'Welcome to Application';
+    }
+
+    /**
+     * Returns an array or a string containing usage information for this module's Console commands.
+     * The method is called with active Zend\Console\Adapter\AdapterInterface that can be used to directly access
+     * Console and send output.
+     *
+     * If the result is a string it will be shown directly in the console window.
+     * If the result is an array, its contents will be formatted to console window width. The array must
+     * have the following format:
+     *
+     *     return array(
+     *                'Usage information line that should be shown as-is',
+     *                'Another line of usage info',
+     *
+     *                '--parameter'        =>   'A short description of that parameter',
+     *                '-another-parameter' =>   'A short description of another parameter',
+     *                ...
+     *            )
+     *
+     * @param AdapterInterface $console
+     * @return array|string|null
+     */
+    public function getConsoleUsage(AdapterInterface $console)
+    {
+        return array(
+            'Place CLI help here',
+        );
     }
 
 }
