988de41921868ecd4e85f9e1777ad12b0af85502
Sergej Kurakin <sergej@kurakin.info>
2013-07-27 10:05:49 +0300

    Add abstract controller between ZF2 and our controllers

---
+++ module/Application/src/Application/Controller/AbstractActionController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController as ZendAbstractActionController;
+
+abstract class AbstractActionController extends ZendAbstractActionController
+{
+
+}
