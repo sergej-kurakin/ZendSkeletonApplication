caab47181223e4250aac4f09769f9b1e9289fa47
Sergej Kurakin <sergej@kurakin.info>
2013-07-28 16:40:57 +0300

    Controller simple test - home route and error 404

---
+++ module/Application/tests/ApplicationTest/Controller/IndexControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ApplicationTest\Controller;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class IndexControllerTest extends AbstractHttpControllerTestCase
+{
+
+    protected $traceError = true;
+
+    public function setUp()
+    {
+
+        $this->setApplicationConfig(
+            include __DIR__.'/../../../../../config/application.config.php'
+        );
+        parent::setUp();
+    }
+
+    public function testIndexActionCanBeAccessed()
+    {
+        $this->dispatch('/');
+        $this->assertResponseStatusCode(200);
+
+        $this->assertModuleName('Application');
+        $this->assertControllerName('Application\Controller\Index');
+        $this->assertControllerClass('IndexController');
+        $this->assertMatchedRouteName('home');
+    }
+
+    public function testError404()
+    {
+        $this->dispatch('/url/does/not/exists');
+        $this->assertResponseStatusCode(404);
+    }
+
+}
